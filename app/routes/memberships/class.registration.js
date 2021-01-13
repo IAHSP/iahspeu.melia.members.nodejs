884380dc70540f03dadef1b624cb9c85e6b5034a
gabe <gabriel@iahsp.com>
2021-01-13 02:07:08 +0000

    progress toward getting file upload working.  but at least cors is working now, and get unapproved users is working successfully

--- app/routes/memberships/class.registration.js
+++ app/routes/memberships/class.registration.js
@@ -6,8 +6,24 @@ class Registration {
     //this.Service = theService;
     this.currentError = "Default Error Text for Registration Class";
   }
+
+  saveUploadedFiles(body, Service) {
+    //
+    console.log('being called from class.registration.js. saveUploadedFiles was called.');
+  }
+
+
+  // this is to handle the form submission
   async createNewUser(userData, Service){
-    try{
+    // recieve the files, and temporarily store them
+    try {
+      //
+    } catch (err) {
+      //
+    }
+
+    // create the user
+    try {
       const myResults = await Service.createNewUser(userData);
       //console.log('myResults: ', myResults);
       return myResults;
--- app/routes/memberships/membershipsRouter.js
+++ app/routes/memberships/membershipsRouter.js
@@ -1,15 +1,40 @@
 const express = require("express");
 const app = express();
 
+const cors = require("cors");
 const bodyParser = require("body-parser");
 
 const membershipsRouter = express.Router();
 
+const multer = require('multer');
+
+const milliToken = Date.now().toString();
+const upload = multer({dest: __dirname + '/uploads/' + milliToken});
 
 const Service = require( './class.service');
 const Registration = require('./class.registration');
 const UnapprovedUsers = require('./class.unapprovedusers');
 const ApproveUser = require('./class.approveuser');
+
+const corsWhiteList = [
+  "https://iahsp.com",
+  "https://www.iahsp.com",
+  "http://localhost:4200"
+]; // corsWhiteList
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    //checking if !origin, is used in case we are testing this using
+    //firebase serve --only functions.  Because then, origin is skipped
+    //https://stackoverflow.com/questions/42589882/nodejs-cors-middleware-origin-undefined
+    if (corsWhiteList.indexOf(origin) !== -1 || !origin) {
+      return callback(null, true);
+    } else {
+      console.log(`${origin} is not allowed by CORS`);
+      return callback(new Error("Not allowed by CORS"));
+    } // if
+  } // origin: function()
+}; // corsOptions
 
 // Express Configuration
 membershipsRouter.use(bodyParser.json());
@@ -23,6 +48,8 @@ let unapprovedUsers = new UnapprovedUsers(Service);
 let approveUser = new ApproveUser(Service);
 //let registration = new Registration(Service);
 
+//membershipsRouter.options('/get_unapproved_users', cors(corsOptions)); // enable pre-flight request
+membershipsRouter.all('*', cors(corsOptions)); // enable pre-flight request
 membershipsRouter.route('/get_unapproved_users')
   .all((req, res, next) => {
     res.setHeader('Content-Type', 'application/json');
@@ -74,12 +101,27 @@ membershipsRouter.route('/register')
     res.setHeader('Content-Type', 'application/json');
     next();
   })
-  .post(async (req, res, next ) => {
-
+  .post(upload.fields(
+    [
+      {
+        name: 'photoProfilePic', maxCount: 1
+      },
+      {
+        name: 'fileCertificate', maxCount: 1
+      }
+    ]), async (req, res, next ) => {
+    console.log('/register successfully triggered for post');
+    console.log(req);
     let finalResults = {
       "status" : false,
       "payload" : null
     }
+
+    Registration.saveUploadedFiles(req.body, Service).then(
+      //
+    ).catch((err) => {
+      console.log('Registration.saveUploadedFiles has failed. because of: ' + err);
+    });
 
     // Determine function successes.
     Registration.createNewUser(req.body, Service)
